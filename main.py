01f94c3877dda8a71e6ccf5583c401bfe8a94d45
Pawel Grabczak <grabczak.pa@gmail.com>
2020-09-27 20:46:51 +0200

    add requests module

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 import time
+import json
 
 
 def main():
+    url = 'http://localhost:8000/private-station'
+    headers = {'content-type': 'application/json'}
     while True:
-
+        payload = {'some': 'data'}
+        r = requests.post(url, data=json.dumps(payload), headers=headers)
         time.sleep(3600)
 
 
