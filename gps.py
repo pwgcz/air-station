b366bb0b75cd9ab22de0103007e6395f6c411234
Pawel Grabczak <grabczak.pa@gmail.com>
2020-09-27 18:37:17 +0200

    test gps

--- gps.py
+++ gps.py
@@ -3,36 +3,42 @@ import serial
 port = "/dev/ttyS0"
 
 
-def decode(coord):
-    v = coord.split(".")
+def decimal_format(coordinate):
+    print(coordinate)
+    v = coordinate.split(".")
+    print(v)
     head = v[0]
     tail = v[1]
     deg = head[0:-2]
     minute = head[-2:]
+    print(head, tail, deg, minute)
 
-    return deg + ' deg' + minute + " minute" + "." + tail
+    return deg + '.' + minute + " minute" + "." + tail
 
 
-def parseGPS(data):
-    print(data[0:6])
+def parse_gps(data):
     decoded_data = data.decode()
     if decoded_data[0:6] == "$GPGGA":
-        print(str(decoded_data))
-        print(type(decoded_data))
         s = decoded_data.split(",")
 
         if s[7] == '0':
             print("no satelite data available")
             return None
-        time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
-        lat = decode(s[2])
-        dir_lat = s[3]
-        lon = decode(s[4])
-        dir_lon = s[5]
-        print(f'Lat:{lat}--({dir_lat})  Lon:{lon}--({dir_lon}) time: {time}')
+        raw_time = s[1]
+        hour = slice(0, 2)
+        minutes = slice(2, 4)
+        seconds = slice(4, 6)
+        time = raw_time[hour] + ":" + raw_time[minutes] + ":" + raw_time[seconds]
+        raw_lat = s[2]
+        raq_lon = s[4]
+        direction_lat = s[3]
+        direction_lon = s[5]
+        geg_lat = decimal_format(raw_lat)
+        geg_lon = decimal_format(raq_lon)
+        print(f'Lat:{geg_lat}--({direction_lat})  Lon:{direction_lon}--({geg_lon}) time: {time}')
 
 
 ser = serial.Serial(port, baudrate=9600, timeout=0.5)
 while True:
     data = ser.readline()
-    parseGPS(data)
+    parse_gps(data)
