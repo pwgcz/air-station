0ef8982712cb79fb2b3a39ad27fac0cc73814e68
Pawel Grabczak <grabczak.pa@gmail.com>
2020-09-28 12:55:12 +0200

    test

--- main.py
+++ main.py
@@ -1,48 +1,14 @@
-
 import json
 import time
-import board
-import busio
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
-from gpiozero import LED
+from dust_sensor import average_measurement, turn_off_led
 
-
-def turn_off_led():
-    led = LED('GPIO16')
-    led.on()
-
-
-def measurement():
-    led = LED('GPIO16')
-    i2c = busio.I2C(board.SCL, board.SDA)
-
-    ads = ADS.ADS1115(i2c)
-    chan0 = AnalogIn(ads, ADS.P0)
-
-    led.off()
-    time.sleep(.00028)
-    raw_data = chan0.voltage
-    led.on()
-
-    data = raw_data * 500
-    return data
-
-
-def average_measurement():
-    measure_list = []
-    for i in range(10):
-        measure_list.append(measurement())
-
-    average = sum(measure_list) / len(measure_list)
-
-    return f'{"%.4f" % average} ug/m3'
 
 
 def main():
     turn_off_led()
-    # url = 'http://localhost:8000/private-station'
-    # headers = {'content-type': 'application/json'}
+
+    url = 'http://localhost:8000/private-station'
+    headers = {'content-type': 'application/json'}
     measure_data = average_measurement()
     print(measure_data)
 
@@ -60,11 +26,10 @@ def main():
             # "gegrLat": geg_lon,
             # "gegrLon": geg_lon,
         }
-        print(measure_data)
-        print(payload)
         print(json.dumps(payload))
         # r = requests.post(url, data=json.dumps(payload), headers=headers)
-        time.sleep(3)
+        time.sleep(3600)
 
 
-main()
+if __name__ == "__main__":
+    main()
